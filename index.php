b00aadb3b8f2e9e68c1164668e5b5f4e42ee6cf7
Kuldeep97164 <jangidkuldeep861@gmail.com>
2026-02-21 15:54:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $get = $_GET['get'];
-$mpdUrl = 'https://b500e47a210c49479f2313d1a124f855.mediatailor.ap-southeast-1.amazonaws.com/v1/dash/ceb8322ae2e84c32cb0fce196fdc60100025ab50/'. $get;
+$mpdUrl = 'https://linearjitp-playback.astro.com.my/dash-wv/dashiso/'. $get;
 
 $mpdheads = [
   'http' => [
